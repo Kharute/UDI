211dd6a7d1a471f08462e4f343aa985501f4a015
김도형 <kidh1238@gmail.com>
2024-06-21 15:34:42 +0900

    [Fixed] 에셋 폴더 위치 변경, UI(재화, 획득UI) 추가

--- Assets/Scripts/View/MenuButtonClick.cs
+++ Assets/Scripts/View/MenuButtonClick.cs
@@ -1,6 +1,7 @@
 
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 
 public class MenuButtonClick : MonoBehaviour
@@ -9,19 +10,38 @@ public class MenuButtonClick : MonoBehaviour
     float moveValue = 300f;
     [SerializeField]
     GameObject[] GameObjects;
+    [SerializeField]
+    float rolltime = 1.0f;
     bool isMenuOpen = false;
 
     public void OnButtonClick_Menu()
     {
-        isMenuOpen = !isMenuOpen;
+        StartCoroutine(MenuStateChange());
+    }
 
+    IEnumerator MenuStateChange()
+    {
+        isMenuOpen = !isMenuOpen;
+        
         float upValue = 0;
         foreach (var go in GameObjects)
         {
-            //gameObject.SetActive(isMenuOpen);
+            if (isMenuOpen)
+            {
+                go.SetActive(true);
+            }
             upValue += isMenuOpen ? 1 : -1;
-            Vector3 vec  = go.transform.position;
-            go.transform.DOMove(vec + Vector3.up * moveValue * upValue, 1f);
+            Vector3 vec = go.transform.position;
+            go.transform.DOMove(vec + Vector3.up * moveValue * upValue, rolltime);
+        }
+        yield return new WaitForSeconds(rolltime);
+
+        foreach (var go in GameObjects)
+        {
+            if (!isMenuOpen)
+            {
+                go.SetActive(false);
+            }
         }
     }
 }
